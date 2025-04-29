0eb8ab78839580385cdbd9fedfeecd6d4da05423
pubuzhixing8 <pubuzhixing@gmail.com>
2025-04-29 21:38:00 +0800

    feat: improve getTextFromNode and buildPlaitMindData

--- playground/index.tsx
+++ playground/index.tsx
@@ -31,11 +31,26 @@ interface TreeNode {
 
 export const getTextFromNode = (node: TreeNode) => {
   let text = "";
-  depthFirstRecursion(node, (node: TreeNode) => {
-    if (node.type === "text") {
+  const transform = (node: TreeNode) => {
+    if (
+      node.type === "text" ||
+      node.type === "inlineCode" ||
+      node.type === "code"
+    ) {
       text += node.value;
+      return;
     }
-  });
+    if (node.type === "link") {
+      text += `${node.url}`;
+      return;
+    }
+    if (node.children && node.children.length > 0) {
+      node.children.forEach((node) => {
+        transform(node);
+      });
+    }
+  };
+  transform(node);
   return text;
 };
 
@@ -58,25 +73,53 @@ export const buildPlaitMindData = (root: TreeNode, board: PlaitBoard) => {
   const mind = createEmptyMind(board, [0, 0]);
   const parentNodeMap: Record<string, MindElement> = { "0": mind };
   let currentParent: MindElement = mind;
-  depthFirstRecursion(root, (node: TreeNode) => {
-    if (node.type === "root") {
-      return;
-    }
+  const transform = (node: TreeNode, isNext = false) => {
     if (node.type === "heading") {
       const parentMindNode = getParentMindNode(node, parentNodeMap);
       const text = getTextFromNode(node);
+      if (!text) {
+        return;
+      }
       const { width, height } = getTopicSize(false, false, buildText(text));
       const element = createMindElement(text, width, height, {});
       parentMindNode.children.push(element);
       parentNodeMap[`${node.depth}`] = element;
       currentParent = element;
     } else if (node.type === "list") {
+      node.children?.forEach((node) => {
+        transform(node);
+      });
     } else if (node.type === "listItem") {
+      if (node.children && node.children.length > 1) {
+        const _currentParent = currentParent;
+        node.children.forEach((node, index) => {
+          transform(node, index === 0);
+        });
+        currentParent = _currentParent;
+        return;
+      }
       const text = getTextFromNode(node);
+      if (!text) {
+        return;
+      }
       const { width, height } = getTopicSize(false, false, buildText(text));
       const element = createMindElement(text, width, height, {});
       currentParent.children.push(element);
+    } else {
+      const text = getTextFromNode(node);
+      if (!text) {
+        return;
+      }
+      const { width, height } = getTopicSize(false, false, buildText(text));
+      const element = createMindElement(text, width, height, {});
+      currentParent.children.push(element);
+      if (isNext) {
+        currentParent = element;
+      }
     }
+  };
+  root.children?.forEach((node) => {
+    transform(node);
   });
   return mind;
 };
@@ -87,9 +130,9 @@ const App = () => {
   const handleOnChange = useCallback(async (markdown: string) => {
     const processor = unified().use(remarkParse);
     const result = processor.parse(markdown);
-    console.log(result);
+    console.log(result, "parse");
     const mindData = buildPlaitMindData(result as TreeNode, boardRef.current!);
-    console.log(mindData);
+    console.log(mindData, "drawnix data");
     setElements([mindData]);
   }, []);
 
